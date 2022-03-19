a1fce321bbcef2231ad1aba1713d6e6c2507ab98
André Gonzalez <lopescg@gmail.com>
2022-03-19 17:08:25 -0300

    changed gaps between windows

--- config.h
+++ config.h
@@ -2,25 +2,25 @@
 
 /* appearance */
 		/* with TV as second monitor */
-static const unsigned int borderpx       = 3;   /* border pixel of windows */
-static const unsigned int snap           = 32;  /* snap pixel */
-static const unsigned int gappih         = 0;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 0;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 70;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 0;  /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
-static const int showbar                 = 1;   /* 0 means no bar */
-static const int topbar                  = 1;   /* 0 means bottom bar */
-		/* With only laptop screen as monitor */
-/* static const unsigned int borderpx       = 4;   /1* border pixel of windows *1/ */
+/* static const unsigned int borderpx       = 3;   /1* border pixel of windows *1/ */
 /* static const unsigned int snap           = 32;  /1* snap pixel *1/ */
 /* static const unsigned int gappih         = 0;  /1* horiz inner gap between windows *1/ */
 /* static const unsigned int gappiv         = 0;  /1* vert inner gap between windows *1/ */
-/* static const unsigned int gappoh         = 0;  /1* horiz outer gap between windows and screen edge *1/ */
+/* static const unsigned int gappoh         = 70;  /1* horiz outer gap between windows and screen edge *1/ */
 /* static const unsigned int gappov         = 0;  /1* vert outer gap between windows and screen edge *1/ */
 /* static const int smartgaps_fact          = 1;   /1* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps *1/ */
-/* static const int showbar                 = 0;   /1* 0 means no bar *1/ */
+/* static const int showbar                 = 1;   /1* 0 means no bar *1/ */
 /* static const int topbar                  = 1;   /1* 0 means bottom bar *1/ */
+		/* With only laptop screen as monitor */
+static const unsigned int borderpx       = 4;   /* border pixel of windows */
+static const unsigned int snap           = 32;  /* snap pixel */
+static const unsigned int gappih         = 0;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 0;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 0;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 0;  /* vert outer gap between windows and screen edge */
+static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int showbar                 = 0;   /* 0 means no bar */
+static const int topbar                  = 1;   /* 0 means bottom bar */
 
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
 static const int statusmon               = 'A';
