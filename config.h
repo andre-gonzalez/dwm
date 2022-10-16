e1e6c8838fdf867679aa3cd980ccf6cbff375015
André Gonzalez <lopescg@gmail.com>
2022-10-16 16:16:52 -0500

    removed configuration when using TV as monitor

--- config.h
+++ config.h
@@ -1,17 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-/* appearance */
-		/* with TV as second monitor */
-/* static const unsigned int borderpx       = 3;   /1* border pixel of windows *1/ */
-/* static const unsigned int snap           = 32;  /1* snap pixel *1/ */
-/* static const unsigned int gappih         = 0;  /1* horiz inner gap between windows *1/ */
-/* static const unsigned int gappiv         = 0;  /1* vert inner gap between windows *1/ */
-/* static const unsigned int gappoh         = 70;  /1* horiz outer gap between windows and screen edge *1/ */
-/* static const unsigned int gappov         = 0;  /1* vert outer gap between windows and screen edge *1/ */
-/* static const int smartgaps_fact          = 1;   /1* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps *1/ */
-/* static const int showbar                 = 1;   /1* 0 means no bar *1/ */
-/* static const int topbar                  = 1;   /1* 0 means bottom bar *1/ */
-		/* With only laptop screen as monitor */
 static const unsigned int borderpx       = 4;   /* border pixel of windows */
 static const unsigned int snap           = 32;  /* snap pixel */
 static const unsigned int gappih         = 0;  /* horiz inner gap between windows */
