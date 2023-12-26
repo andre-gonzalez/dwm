0a918a41130f360f68cacc2566a8e0a1507f6fe0
André Gonzalez <lopescg@gmail.com>
2023-12-26 09:50:42 -0600

    set dbeaver to appear on tag 8

--- config.h
+++ config.h
@@ -169,7 +169,7 @@ static const Rule rules[] = {
     {                 NULL,             NULL,       "WhatsApp",  NULL,    1 << 7,    0,          -1 },
     {            "DBeaver",             NULL,             NULL,  NULL,    1 << 7,    0,          -1 },
     {           "VSCodium",             NULL,             NULL,  NULL,    1 << 8,    0,          -1 },
-    {                 NULL,             NULL,        "Dbeaver",  NULL,    1 << 8,    0,          -1 },
+    {                 NULL,             NULL,        "Dbeaver",  NULL,    1 << 7,    0,          -1 },
 		{                 NULL,             NULL, "Pritunl Client",  NULL,    1 << 8,    0,          -1 },
 		{                 NULL,             NULL,      "Bitwarden",  NULL,    1 << 8,    0,          -1 },
 		{           "Clockify",             NULL,             NULL,  NULL,    1 << 8,    0,          -1 },
