563413e3ef98ca0b28239fa4529f1e91591b2268
André Gonzalez <lopescg@gmail.com>
2024-07-24 17:01:40 -0600

    Set font size from 10 to 9

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const int statusmon               = 'A';
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
-static const char *fonts[]               = { "Liberation Mono:size=10" };
-static const char dmenufont[]            = "Liberation Mono:size=10";
+static const char *fonts[]               = { "Liberation Mono:size=9" };
+static const char dmenufont[]            = "Liberation Mono:size=9";
 
 static char c000000[]                    = "#000000"; // placeholder value
 
