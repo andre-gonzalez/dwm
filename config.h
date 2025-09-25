cebaf987a048147dc4fb5d0a52323975bfaada69
André Gonzalez <lopescg@gmail.com>
2025-09-25 19:27:37 -0300

    Add enteauth keybind

--- config.h
+++ config.h
@@ -285,6 +285,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_o,          spawn,                  SHCMD("/$HOME/.scripts/web") },
 	{ MODKEY,                       XK_a,          spawn,                  SHCMD("/$HOME/.scripts/todo") },
 	{ MODKEY,                       XK_c,          spawn,                  SHCMD("chatgpt-dmenu") },
+	{ MODKEY,                       XK_e,          spawn,                  SHCMD("/usr/bin/enteauth") },
 	{ MODKEY,                       XK_b,          togglebar,              {0} },
 	{ MODKEY,                       XK_j,          focusstack,             {.i = +1 } },
 	{ MODKEY,                       XK_k,          focusstack,             {.i = -1 } },
