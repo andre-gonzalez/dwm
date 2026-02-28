13245896087bd90dceef1a7d2875f9a955cfa3b8
André Gonzalez <lopescg@gmail.com>
2026-02-28 18:22:42 -0300

    Update the name of bitwarden dmenu script

--- config.h
+++ config.h
@@ -265,7 +265,7 @@ static Key keys[] = {
 	/* { MODKEY|ShiftMask,             XK_Return,     spawn,                  {.v = termcmd } }, */
 	{ MODKEY|ShiftMask,             XK_v,          spawn,                  SHCMD("clipmenu") },
 	{ MODKEY|ControlMask,           XK_v,          spawn,                  SHCMD("/$HOME/.scripts/copy-to-clipboard.sh ") },
-	{ MODKEY,                       XK_s,          spawn,                  SHCMD("bd") },
+	{ MODKEY,                       XK_s,          spawn,                  SHCMD("bw-dmenu") },
 	{ MODKEY|ShiftMask,             XK_s,          spawn,                  SHCMD("flameshot gui -c -p $HOME/gdrive-pessoal/downloads/screenshots/") },
 	{ MODKEY|ControlMask,           XK_s,          spawn,                  SHCMD("$HOME/.scripts/print-to-markdown-note") },
 	{ MODKEY|ControlMask,           XK_k,          spawn,                  SHCMD("$HOME/.scripts/dmenu-kill-processes.sh") },
