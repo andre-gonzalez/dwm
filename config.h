65294f0beb434bba51f21bad25d829e802a1a964
André Gonzalez <lopescg@gmail.com>
2026-03-20 10:26:44 -0300

    Fix/ pavucontrol keybind also opening a terminal

--- config.h
+++ config.h
@@ -312,7 +312,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_b,          spawn,                  SHCMD("$HOME/.scripts/conectar-xm3") },
 	{ MODKEY,                       XK_F1,         spawn,                  SHCMD("playerctl play-pause") },
 	{ ControlMask|ShiftMask,        XK_Escape,     spawn,                  SHCMD("st -e btop") },
-	{ MODKEY|ShiftMask,             XK_a,          spawn,                  SHCMD("st -e pavucontrol") },
+	{ MODKEY|ShiftMask,             XK_a,          spawn,                  SHCMD("pavucontrol") },
 	{ MODKEY,                       XK_z,          spawn,                  SHCMD("setxkbmap -layout us -variant dvorak-intl && setxkbmap -option caps:escape") },
 	{ MODKEY,                       XK_Z,          spawn,                  SHCMD("setxkbmap -layout us -variant dvorak-intl && setxkbmap -option caps:escape") },
 	{ MODKEY,                       XK_o,          spawn,                  SHCMD("/$HOME/.scripts/web") },
