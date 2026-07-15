221c34b7e3f9bb50755534f5670fd1b79c08dd01
André Gonzalez <lopescg@gmail.com>
2026-07-15 18:25:56 -0300

    Add tag rule for Whispering

--- config.h
+++ config.h
@@ -224,6 +224,7 @@ static const Rule rules[] = {
     {                 NULL,             NULL,        "Dbeaver",  NULL,    1 << 7,    0,          -1 },
     { "jetbrains-datagrip",             NULL,             NULL,  NULL,    1 << 7,    0,          -1 },
 	{                 NULL,             NULL, "Pritunl Client",  NULL,    1 << 8,    0,          -1 },
+	{                 NULL,             NULL,     "Whispering",  NULL,    1 << 8,    0,           0 },
 	{     "AWS VPN Client",             NULL,             NULL,  NULL,    1 << 8,    0,          -1 },
 	{                 NULL,             NULL,      "Bitwarden",  NULL,    1 << 8,    0,          -1 },
 	{                 NULL,             NULL,      "Bitwarden",  NULL,    1 << 8,    0,          -1 },
